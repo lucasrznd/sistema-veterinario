04b7e097e44b9b04c9d19596665d2b721dafcfb0
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-14 14:09:24 -0300

    feat: add email on footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,8 +2,16 @@ export default function Footer() {
     const anoAtual = new Date().getFullYear();
 
     return (
-        <div className="flex align-items-center justify-content-center bg-primary border-round mt-2 h-4rem">
-            <p className="text-md font-bold">VetConsent - {anoAtual}.&copy;</p>
-        </div>
+        <>
+            <div className="flex align-items-center justify-content-center bg-primary border-round mt-2 h-4rem">
+                <p className="text-md font-bold">VetConsent - {anoAtual}.&copy;</p>
+            </div>
+
+            <div className="flex align-items-center justify-content-center">
+                <p>Para maiores informações, entre em contato com o suporte:
+                    <a href="mailto:suporte@vetconsent.com.br"><strong> suporte@vetconsent.com.br</strong></a>
+                </p>
+            </div>
+        </>
     )
 }
