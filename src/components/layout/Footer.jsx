70511acaea8f019004588e7933b5cf46d12c6c73
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-14 13:35:33 -0300

    refactor: current year on footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
+    const anoAtual = new Date().getFullYear();
+
     return (
         <div className="flex align-items-center justify-content-center bg-primary border-round mt-2 h-4rem">
-            <p className="text-md font-bold">VetSurge - 2024.&copy;</p>
+            <p className="text-md font-bold">VetSurge - {anoAtual}.&copy;</p>
         </div>
     )
 }
