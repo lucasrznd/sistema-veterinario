ed705e601b9590936701c0c44e330531c1adeb3e
Lucas Rezende <lucasffrezende@gmail.com>
2025-03-27 21:59:27 -0300

    feat: replace Layout and Home components with AppRoutes for improved routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import 'primeflex/primeflex.css'; // flex
 import 'primeicons/primeicons.css'; //icons
 import 'primereact/resources/primereact.min.css'; //core css
 import 'primereact/resources/themes/lara-light-amber/theme.css'; //theme
-import Layout from "./components/layout/Layout";
-import Home from "./pages/Home";
+import AppRoutes from "./routes/Routes";
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <AppRoutes />
   )
 }
 
