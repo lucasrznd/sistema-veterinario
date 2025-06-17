dd720c77a48e1ae9f40ddc99df5731805f69763f
Lucas Rezende <lucasffrezende@gmail.com>
2025-06-17 08:45:37 -0300

    chore: add AppRoutes on root

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import 'primereact/resources/themes/lara-light-amber/theme.css'; //theme
-import 'primereact/resources/primereact.min.css'; //core css
-import 'primeicons/primeicons.css'; //icons
-import 'primeflex/primeflex.css'; // flex
-import Home from "./pages/Home";
-import Layout from "./components/layout/Layout";
-import ManutencaoPage from './pages/Manutencao';
+import "primereact/resources/themes/lara-light-amber/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css"; // flex
+import AppRoutes from "./routes/Routes";
 
 function App() {
-  return (
-    <ManutencaoPage />
-    // <Layout>
-    //   <Home />
-    // </Layout>
-  )
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LoginPage from "../pages/Login";
-import Layout from '../components/layout/Layout';
+import Layout from "../components/layout/Layout";
 import Home from "../pages/Home";
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
-  
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+
   return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 const AuthRedirect = () => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
-  
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+
   return isAuthenticated ? <Navigate to="/home" replace /> : <LoginPage />;
 };
 
@@ -22,7 +22,14 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<AuthRedirect />} />
         <Route element={<ProtectedRoute />}>
-          <Route path="/home" element={<Layout><Home /></Layout>} />
+          <Route
+            path="/home"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
         </Route>
       </Routes>
     </Router>
